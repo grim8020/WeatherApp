dc51a242e77f55431184c9ea99e7339694e6e133
Shane Grim <grim8020@gmail.com>
2022-04-22 11:23:15 -0400

    Added search and text field

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -22,14 +22,36 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         searchTextField.delegate = self
     }
 
+    @IBAction func searchPressed(_ sender: UIButton) {
+        //this action tells the search field that we are done editing and should dismiss the keyboard
+        searchTextField.endEditing(true)
+        print(searchTextField.text!)
+    }
+    
     //return button gets pressed -> what should it do.  Must return a true or fasle (Bool) at the end of the function to determine if it should proccess that action
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        //this action tells the search field that we are done editing and should dismiss the keyboard
+        searchTextField.endEditing(true)
         print(searchTextField.text!)
         return true
     }
-
-    @IBAction func searchPressed(_ sender: UIButton) {
-        print(searchTextField.text!)
+    
+    //what happens when the user deselects the text field without typing something
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Type something"
+            return false
+        }
+    }
+    
+    //clears the search bar after word is entered to make another search without having to delete the previous word
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //use search textField.text to get the weather for that city
+        
+        
+        searchTextField.text = ""
     }
     
 }
