9a2f682d0e7184a4bc7b464d676a1de17889f66b
Shane Grim <grim8020@gmail.com>
2022-04-22 09:16:06 -0400

    Added Xcode project

---
+++ Clima/Controller/WeatherViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Clima
+//
+//  Created by Angela Yu on 01/09/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class WeatherViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var searchTextField: UITextField!
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //text field should refer back to viewcontroller
+        searchTextField.delegate = self
+        
+        
+        
+        
+    }
+
+
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
+    
+}
+
